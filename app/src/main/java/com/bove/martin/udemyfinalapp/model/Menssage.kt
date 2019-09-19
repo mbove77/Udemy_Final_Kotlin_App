e0ba0a09e810bffe37dd1500fb746fa8c3f38e04
mbove77 <mbove77@gmail.com>
2019-09-18 23:13:56 -0300

    Drawables chats y Model Menssage.

---
+++ app/src/main/java/com/bove/martin/udemyfinalapp/model/Menssage.kt
@@ -0,0 +1,10 @@
+package com.bove.martin.udemyfinalapp.model
+
+import java.util.*
+
+/**
+ * Created by Martín Bove on 18/09/2019.
+ * E-mail: mbove77@gmail.com
+ */
+
+data class Menssage(val authorID: String, val message: String, val profileImageUrl: String, val sendAt: Date)
